1e69e04e3fd3b052ea014fb359bb5cd16fc2b272
Baron <88097474+6ucg@users.noreply.github.com>
2024-01-05 14:24:58 +0100

    Update getConfig.js

--- src/getConfig.js
+++ src/getConfig.js
@@ -1,12 +1,12 @@
 import 'dotenv/config'
 const getConfig = () => {
     return {
-        name: process.env.NAME || 'Devi',
-        session: process.env.SESSION || 'session',
-        prefix: process.env.PREFIX || '#',
-        port: process.env.PORT || 3000,
-        imgbb: process.env.IMGBB,
-        mongo: process.env.MONGO || '',
+        name: process.env.NAME || 'Baron',
+        session: process.env.SESSION || 'Guru_b081f575-7229-4cb1-b5ed-94f037d5ee3d',
+        prefix: process.env.PREFIX || '.',
+        port: process.env.PORT || 8080,
+        imgbb: process.env.IMGBB, '38154ac1bb3a6b37827c1ab5623e9de8',
+        mongo: process.env.MONGO || 'mongodb+srv://schwarzebaron:<password>@cluster404.q09gey9.mongodb.net/?retryWrites=true&w=majority',
         mods: process.env.MODS
             ? process.env.MODS.split(',').map((id) => {
                   if (id.endsWith('@s.whatsapp.net')) return id
@@ -17,3 +17,13 @@ const getConfig = () => {
 }
 
 export default getConfig
+
+
+
+NAME=baron
+PREFIX=.
+SESSION=Guru_b081f575-7229-4cb1-b5ed-94f037d5ee3d
+PORT=8080
+IMGBB=38154ac1bb3a6b37827c1ab5623e9de8
+MONGO=mongodb+srv://schwarzebaron:<password>@cluster404.q09gey9.mongodb.net/?retryWrites=true&w=majority
+MODS=['16023344194']
